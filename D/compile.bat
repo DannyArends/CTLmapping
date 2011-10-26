1542738d993ee0bad52dfdc7d0ae5fc07ee27b5a
DannyArends <Danny.Arends@gmail.com>
2011-10-26 23:06:13 +0200

    moving the dll to test

--- D/compile.bat
+++ D/compile.bat
@@ -1,5 +1,6 @@
 call setpath d
-rm qcl.dll
+del qcl.dll
 dmd -ofqcl.dll -L/IMPLIB  -O -inline -release qcl.d dll\dllmain.d dll\raux.d libs\libload.d libs\r.d dll\mydll.def phobos.lib -IC:\D\dmd2\windows\bin
-rm ATA.lib
-rm qcl.obj
+move qcl.dll ../tests
+del ATA.lib
+del qcl.obj
