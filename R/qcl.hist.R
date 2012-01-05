1677b35d91de25275f41da3f983490782ee97842
DannyArends <Danny.Arends@gmail.com>
2012-01-05 14:37:43 +0100

    Minor bugin hist plotting

--- R/qcl.hist.R
+++ R/qcl.hist.R
@@ -11,11 +11,11 @@
 hist.QCLobject <- function(x, pheno.col=1, ...){
   if(missing(x)) stop("argument 'x' which expects a 'QCLobject' object is missing, with no default")
   namez <- NULL
-  plot(c(0,0.8),c(0,length(unlist(x[[pheno.col[1]]]$p))/5),type='n',main="QCL scores permutations",ylab="Frequency",xlab="Difference in correlation^2")
+  plot(c(0,1.0),c(0,length(unlist(x[[pheno.col[1]]]$p))/5),type='n',main="QCL scores permutations",ylab="Frequency",xlab="Difference in correlation^2")
   for(pheno in pheno.col){
     if(is.null(x[[pheno]]$p)) stop(paste("Permutations not found for pheno.col=",pheno))
     sorted <- sort(unlist(x[[pheno]]$p))
-    hist(sorted, breaks=seq(0,0.8,0.01), add=TRUE, col=pheno, ...)
+    hist(sorted, breaks=seq(0,1.0,0.01), add=TRUE, col=pheno, ...)
     namez <- c(namez,attr(x[[pheno]]$qcl,"name"))
   }
   legend("topright", namez, col=pheno.col,lwd=6)
