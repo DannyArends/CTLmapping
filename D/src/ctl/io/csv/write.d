e391ad8df69204524fc60284d6a646bfa3c25d85
DannyArends <Danny.Arends@gmail.com>
2012-02-08 10:34:48 +0100

    --format, --overwrite

--- D/src/ctl/io/csv/write.d
+++ D/src/ctl/io/csv/write.d
@@ -14,23 +14,28 @@ import std.conv;
 
 import ctl.core.array.matrix;
 
-void writeFile(T)(T[][] m, string filename, bool verbose = false){
+void writeFile(T)(T[][] m, string filename, bool overwrite = false, bool verbose = false){
   if(exists(filename)){
-    writefln("Already exists %s",filename);
-  }else{
-    try{
-      auto fp = new File(filename,"wb");
-      string      buffer;
-      for (int r=0; r<m.length; r++) {
-        for (int c=0; c<m[r].length; c++) {
-          if(c!=0) fp.write("\t");
-          fp.write(to!string(m[r][c]));
-        }
-        fp.writeln();
-      }
-      fp.close();
-    }catch(Throwable e){
-      writefln("File %s read exception: %s", filename,e);
+    if(overwrite){
+      writefln(" - Overwriting file %s",filename);
+      remove(filename);
+    }else{
+      writefln(" - File already exists %s",filename);
+      return;
     }
+  }
+  try{
+    auto fp = new File(filename,"wb");
+    string      buffer;
+    for (int r=0; r<m.length; r++) {
+      for (int c=0; c<m[r].length; c++) {
+        if(c!=0) fp.write("\t");
+        fp.write(to!string(m[r][c]));
+      }
+      fp.writeln();
+    }
+    fp.close();
+  }catch(Throwable e){
+    writefln("File %s read exception: %s", filename,e);
   }
 }
