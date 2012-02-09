b33fd15b4f79c34362fd5713a87807a725fb9371
DannyArends <Danny.Arends@gmail.com>
2012-02-09 10:17:59 +0100

    Updates to mapctl, to map CTL and QTL

--- D/compile.bat
+++ D/compile.bat
@@ -1,5 +1,7 @@
 dmd -lib -O -ofarray.lib src/ctl/core/array/matrix.d src/ctl/core/array/search.d src/ctl/core/array/ranges.d
 dmd -lib -O -ofstats.lib src/ctl/core/stats/basic.d src/ctl/core/stats/correlation.d src/ctl/core/stats/tolod.d -Isrc/
+dmd -lib -O -ofqtl.lib src/ctl/core/stats/qtl.d src/ctl/core/stats/regression.d src/ctl/core/stats/utils.d src/ctl/core/stats/LUdecomp.d src/ctl/core/stats/nrc.d -Isrc/
 dmd -lib -O -ofinput.lib src/ctl/io/csv/parse.d src/ctl/io/csv/write.d src/ctl/io/cmdline/parse.d -Isrc/
 dmd -lib -O -ofctl.lib src/ctl/core/ctl/mapping.d src/ctl/core/ctl/permutation.d src/ctl/core/ctl/utils.d -Isrc/
+dmd -O src/ctl/mapctl.d array.lib stats.lib input.lib ctl.lib qtl.lib -Isrc/
 dmd -O src/ctl/mapctl.d array.lib stats.lib input.lib ctl.lib qtl.lib -Isrc/
