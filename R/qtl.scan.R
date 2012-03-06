f4fff35e2766b91e0ca1e518e2f5361f6c70b5bd
DannyArends <Danny.Arends@gmail.com>
2012-03-06 11:05:24 +0100

    Fast QTL

--- R/qtl.scan.R
+++ R/qtl.scan.R
@@ -8,44 +8,70 @@
 # R functions to do QTL mapping
 #
 
-#-- QTLscan main function --#
-
-QTLscan <- function(genotypes, phenotypes, pheno.col = 1:ncol(phenotypes), verbose = FALSE){
-  if(missing(genotypes)) stop("genotypes are missing")
-  if(missing(phenotypes)) stop("phenotypes are missing")
-  results <- NULL
-  rnames <- NULL
-  cnt <- 1
-  for(x in pheno.col){
-    ss <- proc.time()
-    results <- rbind(results,apply(genotypes,2, 
-      function(geno){
-        linmod <- lm(phenotypes[,x] ~ geno)
-        -log10(anova(linmod)[[5]][1])
-      }
-    ))
-    ee <- proc.time()
-    if(verbose){
-      cat("  - QTLscan of",colnames(phenotypes)[x],"took",as.numeric(ee[3]-ss[3]),"seconds\n")
-    }
-    rnames <- c(rnames,colnames(phenotypes)[x])
-    cnt <- cnt +1
-  }
-  rownames(results) <- rnames
-  class(results) <- c(class(results),"QTLscan")
-  results
+#Recreate a matrix from the per marker modeling results
+create.matrix <- function(models, what="qtl", phenonames, genonames, do.log=TRUE){
+  res <- matrix(unlist((lapply(models,"[",what))),length(phenonames),length(genonames))
+  rownames(res) <- phenonames
+  colnames(res) <- genonames
+  if(do.log) res <- -log10(res)
+  res
 }
 
+#-- QTLscan main function --#
+map.fast <- function(marker, phenotypes, conditions, verbose = FALSE){
+  st  <- proc.time()
+  res <- NULL
+  if(is.null(conditions)){
+    models  <- aov(as.matrix(phenotypes) ~ marker)
+    modelinfo <- summary(models)
+    res$qtl <- unlist(lapply(modelinfo,"[",1,5),use.names=T)
+    res$eff <- unlist(models$coefficients[2,])
+  }else{
+    models  <- aov(as.matrix(phenotypes) ~ conditions + marker + conditions:marker)
+    modelinfo <- summary(models)
+    res$env <- unlist(lapply(modelinfo,"[",1,5),use.names=T)
+    res$qtl <- unlist(lapply(modelinfo,"[",2,5),use.names=T)
+    res$int <- unlist(lapply(modelinfo,"[",3,5),use.names=T)
+    res$eff <- unlist(models$coefficients[2,])
+  }
+  if(verbose) cat("Done in:",(proc.time()-st)[3],"seconds\n")
+  res
+}
+
+#-- QTLscan main function --#
+QTLscan <- function(genotypes, phenotypes, conditions=NULL, n.core=2, verbose = TRUE){
+  if(missing(genotypes)) stop("genotypes are missing")
+  if(missing(phenotypes)) stop("phenotypes are missing")
+  st  <- proc.time()
+  if("snow" %in% rownames(installed.packages())){
+    require("snow")
+    cl <- snow::makeCluster(n.core)
+    models <- snow::parApply(cl, genotypes, 2, "map.fast", phenotypes, conditions)
+    snow::stopCluster(cl)
+  }else{
+    models <- apply(genotypes, 2, "map.fast", phenotypes, conditions)  
+  }
+  res <- NULL
+  res$qtl <- create.matrix(models, "qtl", colnames(phenotypes), colnames(genotypes))
+  res$eff <- create.matrix(models, "eff", colnames(phenotypes), colnames(genotypes), FALSE)
+  if(!is.null(conditions)){
+    res$env <- create.matrix(models, "env", colnames(phenotypes), colnames(genotypes))
+    res$int <- create.matrix(models, "int", colnames(phenotypes), colnames(genotypes))
+  }
+  if(verbose)cat("Done in:",(proc.time()-st)[3],"seconds\n")
+  res
+  class(res) <- c(class(res),"QTLscan")
+  res
+}
 
 #-- R/qtl interface --#
-QTLscan.cross <- function(cross, pheno.col, verbose = FALSE){
+QTLscan.cross <- function(cross, pheno.col, conditions=NULL, n.core=2, verbose = FALSE){
   if(missing(cross)) stop("argument 'cross' is missing, with no default")
   if(has_rqtl()){
     require(qtl)
     phenotypes <- apply(qtl::pull.pheno(cross),2,as.numeric)
-    if(missing(pheno.col)) pheno.col <- 1:ncol(phenotypes)
     genotypes <- qtl::pull.geno(cross)
-    QTLscan(genotypes, phenotypes, pheno.col, verbose)
+    QTLscan(genotypes, phenotypes, conditions, n.core, verbose)
   }else{
     warning(.has_rqtl_warnmsg)
   }
