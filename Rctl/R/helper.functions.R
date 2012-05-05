341f1ec7f0fe97e1b9c7f853d14b036b92fe7478
DannyArends <Danny.Arends@gmail.com>
2012-05-05 14:39:00 +0200

    [Rctl] helper.functions update

--- Rctl/R/helper.functions.R
+++ Rctl/R/helper.functions.R
@@ -3,11 +3,19 @@
 # - Remove the diagonal from a matrix
 # - Chromosome edge locations
 # - Color range for plots (Red-Black-Blue)
-name <- function(CTLobject){ return(attr(CTLobject$ctl,"name")); }
+
+ctl.name <- function(CTLobject){ return(attr(CTLobject$ctl,"name")); }
+
 remove.diag <- function(x){ return(x*lower.tri(x) + x*upper.tri(x)); }
-get.chredges <- unlist(lapply(unique(map_info[,1]),function(x){max(which(map_info[,1]==x));}))
-up <- abs(seq(-2,-0,0.1))/2 ; dw <- seq(0.1,2,0.1)/2
-redblue <- c(rgb(up,0,0), rgb(0,0,dw))
+
+get.chr.edges <- function(map_info){
+  unlist(lapply(unique(map_info[,1]),function(x){max(which(map_info[,1]==x));}))
+}
+
+#Red blue color range
+up <- function(){abs(seq(-2,-0,0.1))/2} 
+dw <- function(){seq(0.1,2,0.1)/2}
+redblue <- function(){c(rgb(up,0,0), rgb(0,0,dw))}
 
 #Helper function to get the top-correlated metabolites
 top.correlated <- function(x){
@@ -18,3 +26,4 @@ top.correlated <- function(x){
   colnames(ret) <- c("top.correlated","id","correlation")
   return(ret)
 }
+
