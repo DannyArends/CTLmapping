9a53d2b72bba3b62dc8ca2372ba80e3285e174f9
DannyArends <Danny.Arends@gmail.com>
2012-05-05 14:51:27 +0200

    [Rctl] Fixing metabolites -> phenotypes and some other minor errors

--- Rctl/R/basic.qc.R
+++ Rctl/R/basic.qc.R
@@ -10,37 +10,38 @@
 
 ### Section: Input data quality control ###
 
-basic.qc <- function(genotypes, phenotypes){
+basic.qc <- function(genotypes, phenotypes, map_info){
   png("QC_genetic_markers.png",width=2000,height=1000)
   image(1:ncol(genotypes),1:ncol(genotypes),cor(genotypes=="A",use="pair"),
         main="Correlation genetic markers",xlab="Markers",ylab="Markers",col=gray.colors(75))
-  abline(h=(get.chredges+.5),col="white")
-  abline(v=(get.chredges+.5),col="white"); box()
+  abline(h=(get.chr.edges(map_info)+.5),col="white")
+  abline(v=(get.chr.edges(map_info)+.5),col="white"); box()
   dev.off()
 
   #Create the Two correlation matrices
-  cor_metabolites <- cor(metabolites,use='pair')
-  rownames(cor_metabolites) <- colnames(metabolites)
-  colnames(cor_metabolites) <- colnames(metabolites)
-  cor_individuals <- cor(t(metabolites),use='pair')
-  rownames(cor_individuals) <- rownames(metabolites)
-  colnames(cor_individuals) <- rownames(metabolites)
+  cor_phenotypes <- cor(phenotypes,use='pair')
+  rownames(cor_phenotypes) <- colnames(phenotypes)
+  colnames(cor_phenotypes) <- colnames(phenotypes)
+  cor_individuals <- cor(t(phenotypes),use='pair')
+  rownames(cor_individuals) <- rownames(phenotypes)
+  colnames(cor_individuals) <- rownames(phenotypes)
 
-  png("QC_Metabolites.png",width=2000,height=1000)
-  image(x=1:ncol(metabolites),y=1:ncol(metabolites),cor_metabolites ,breaks=seq(-1,1.05,0.05),col=redblue)
-  abline(h=(1:ncol(metabolites)+.5),col="white",lwd=0.1)
-  abline(v=(1:ncol(metabolites)+.5),col="white",lwd=0.1); box()  
+  png("QC_Phenotypes.png",width=2000,height=1000)
+  image(x=1:ncol(phenotypes),y=1:ncol(phenotypes),cor_phenotypes ,breaks=seq(-1,1.05,0.05),col=redblue)
+  abline(h=(1:ncol(phenotypes)+.5),col="white",lwd=0.1)
+  abline(v=(1:ncol(phenotypes)+.5),col="white",lwd=0.1); box()  
   dev.off()
 
   png("QC_Individuals.png",width=1000,height=600)
-  image(x=1:nrow(metabolites),y=1:nrow(metabolites),cor_individuals ,breaks=seq(-1,1.05,0.05),col=redblue)
+  image(x=1:nrow(phenotypes),y=1:nrow(phenotypes),cor_individuals ,breaks=seq(-1,1.05,0.05),col=redblue)
   dev.off()
 
   png("QC_Histogram.png",width=1000,height=600)
-  hist(cor_metabolites, xlab="Correlation",main="Correlation: Metabolites (Red), Individuals (Blue)", breaks=seq(-1,1,0.1), col=rgb(1,0,0,0.5),freq=FALSE)
+  hist(cor_phenotypes, xlab="Correlation",main="Correlation: Phenotypes (Red), Individuals (Blue)", 
+       breaks=seq(-1,1,0.1), col=rgb(1,0,0,0.5),freq=FALSE)
   hist(cor_individuals, breaks=seq(-1,1,0.1), add=TRUE,col=rgb(0,0,1,0.5),freq=FALSE)
   dev.off()
 
-  top.correlated(cor_metabolites)[1:10,]
+  invisible(top.correlated(cor_phenotypes))
 }
 
