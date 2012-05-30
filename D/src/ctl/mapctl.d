3205cc84d3e4b28b06b27b4f63221bc6ec04b517
DannyArends <Danny.Arends@gmail.com>
2012-05-30 20:23:28 +0200

    [DCTL] Reworking output

--- D/src/ctl/mapctl.d
+++ D/src/ctl/mapctl.d
@@ -1,32 +1,27 @@
-/**********************************************************************
- * src/ctl/mapctl.d
+/******************************************************************//**
+ * \file ctl/mapctl.d
+ * \brief Function holding the main mapctl workflow
  *
- * copyright (c) 2012 Danny Arends
- * last modified May, 2012
- * first written May, 2011
- **********************************************************************/ 
-import std.stdio;
-import std.math;
-import std.conv;
-import std.file;
-import std.datetime;
-
+ * <i>Copyright (c) 2012</i>GBIC - Danny Arends<br>
+ * Last modified May, 2012<br>
+ * First written May, 2011<br>
+ * Written in the D Programming Language (http://www.digitalmars.com/d)
+ **********************************************************************/
+import std.stdio, std.math, std.conv, std.file, std.datetime;
 import ctl.core.array.matrix;
-import ctl.core.stats.basic;
-import ctl.core.stats.tolod;
+import ctl.core.stats.basic, ctl.core.stats.tolod;
 import ctl.core.analysis;
-import ctl.core.ctl.mapping;
-import ctl.core.ctl.permutation;
-import ctl.io.reader;
+import ctl.core.ctl.mapping, ctl.core.ctl.permutation;
+import ctl.io.reader, ctl.io.terminal;
 import ctl.io.cmdline.parse;
-import ctl.io.csv.write;
+import ctl.io.csv.write, ctl.io.csv.parse;
 
 void main(string[] args){
   SysTime stime = Clock.currTime();
-  writeln("mapCTL: Correlated Trait Locus (CTL) mapping in D");
-  writeln("(c) 2012 written by Danny Arends in the D programming language");
+  MSG("Correlated Trait Locus (CTL) mapping in D");
+  MSG("(c) 2012 written by Danny Arends in the D programming language");
   CTLsettings settings   = parseCmd(args);
-  Reader r = initialize(settings);
+  Reader ireader  = initialize(settings);
   bool verbose    = settings.getBool("--verbose");
   bool overwrite  = settings.getBool("--overwrite");
   string output   = settings.getString("--output");
@@ -34,53 +29,57 @@ void main(string[] args){
   string input_p  = settings.getString("--phenotypes");
   string input_g  = settings.getString("--genotypes");
 
-  if(overwrite) writeln("[overwrite] Overwriting files in on");
-  if(verbose){
-    writefln("[verbose] on");
-    writefln("[output] Output saved to: " ~ output);
-  }
+  if(overwrite) WARN("Overwriting files in on");
+  if(verbose){ MSG("Verbose mode on"); MSG("Output saved to: " ~ output); }
   if(!settings.displayHelp()){
-    writefln("[load] Start with loading input files (%s, %s)",  input_p, input_g);
-    double[][]  phenotypes = r.loadphenotypes(input_p);
-    int[][]     genotypes  = r.loadgenotypes(input_g);
+    MSG("Start loading input files (%s, %s)",  input_p, input_g);
+    double[][]  phenotypes = ireader.loadphenotypes(input_p);
+    int[][]     genotypes  = ireader.loadgenotypes(input_g);
     if(verbose){
-      writefln("[info] %s geno- and %s phenotypes", genotypes.length, phenotypes.length);
-      writefln("[info] Measurements on %s and %s individuals\n", genotypes[0].length, phenotypes[0].length);
+      MSG("Dataset: %s geno- and %s phenotypes", genotypes.length, phenotypes.length);
+      if(genotypes.length == 0){ ERR("No genotypes loaded, analysis aborted"); return; }
+      if(phenotypes.length == 0){ ERR("No phenotypes loaded, analysis aborted"); return; }
+      MSG("Dataset: Measurements on %s and %s individuals\n", genotypes[0].length, phenotypes[0].length);
     }
-
-    assert(genotypes[0].length == phenotypes[0].length, "mismatch between individuals");
+    if(genotypes[0].length != phenotypes[0].length){ ERR("Mismatch between individuals %s != %s", genotypes[0].length, phenotypes[0].length); return; }
     //We need an output path default to ./ ??
     if(!exists(output)) mkdirRecurse(output);
 
     //Start by mapping all QTL
     if(needanalysis(output ~ "/qtls.txt",overwrite)){
-      Analysis a = getanalysis(settings);
-      double[][] result  = a.analyse(genotypes, phenotypes, [], verbose);
+      Analysis analysis = getanalysis(settings);
+      double[][] result = analysis.analyse(genotypes, phenotypes, [], verbose);
       writeFile(result, output ~ "/qtls.txt", overwrite, verbose);
-    }else{ writeln("\n[SKIP] QTL mapping"); }
-    //Do the CTL
-    double[][][] ctlmmatrix;
-    for(uint p=0; p < phenotypes.length; p++){
-      if(verbose) write("-Phenotype ",p);
-      double[][] score;
-      double[][] perms;
-      
-      if(needanalysis(output ~ "/ctl"~to!string(p)~".txt",overwrite)){
-        score = mapping(phenotypes,  genotypes, p, verbose);
-        writeFile(translate(score),  output ~ "/ctl"~to!string(p)~".txt", overwrite, verbose);
-      }else{ writeln("\n[SKIP] CTL mapping"); }
-      
-      if(needanalysis(output ~ "/perms"~to!string(p)~".txt",overwrite)){
+    }else{ WARN("Skipped QTL mapping"); }
+
+    for(uint p=0; p < phenotypes.length; p++){ //Main CTL mapping loop
+      if(verbose) MSG("- Phenotype %s -",p);
+      double[][] ctllod, score, perms;
+      string fn_ctl  = output ~ "/ctl"~to!string(p)~".txt";
+      string fn_perm = output ~ "/perms"~to!string(p)~".txt";
+      string fn_lods = output ~ "/lodscores"~to!string(p)~".txt";
+
+      if(needanalysis(fn_ctl,overwrite)){
+        score = mapping(phenotypes,  genotypes, p, false);
+        writeFile(translate(score),  fn_ctl, overwrite, verbose);
+      }else{ 
+        score = parseFile!double(fn_ctl, false, false);
+        MSG("Skipped CTL mapping, file %s exists", fn_ctl); }
+
+      if(needanalysis(fn_perm,overwrite)){
         perms = permutation(phenotypes, genotypes, p, settings.getInt("--nperms"), verbose);
-        writeFile(translate(perms),  output ~ "/perms"~to!string(p)~".txt", overwrite, verbose);
-      }else{ writeln("\n[SKIP] Permutations"); }
-      
-      if(needanalysis(output ~ "/lodscores"~to!string(p)~".txt",overwrite)){
-        ctlmmatrix  ~= tolod(score, perms, verbose);
-        writeFile(translate(ctlmmatrix[p]),  output ~ "/lodscores"~to!string(p)~".txt", overwrite, verbose);
-      }else{ writeln("\n[SKIP] LOD transformation"); }
+        writeFile(translate(perms), fn_perm, overwrite, verbose);
+      }else{
+        perms = parseFile!double(fn_ctl, false, false);
+        MSG("Skipped permutations, file %s exists", fn_perm); }
+
+      if(needanalysis(fn_lods,overwrite)){
+        ctllod = tolod(score, perms, verbose);
+        writeFile(translate(ctllod), fn_lods, overwrite, verbose);
+      }else{ MSG("Skipped LOD transformation, file %s exists", fn_lods); }
     }
-    writeln("\nmapCTL finished analysis took: ",(Clock.currTime()-stime).total!"seconds"()," seconds");
-    writefln("Continue by starting R and loading the results:\n library(ctl)\n ctls <- load.ctl(\"%s\", \"%s\", \"%s\")\n plot(ctls)",input_g, input_p,output);
+    writeln();
+    MSG("mapCTL finished (%s seconds)",(Clock.currTime()-stime).total!"seconds"()," seconds");
+    MSG("Continue by starting R and loading the results:\n library(ctl)\n ctls <- load.ctl(\"%s\", \"%s\", \"%s\")\n plot(ctls)",input_g, input_p,output);
   }
 }
