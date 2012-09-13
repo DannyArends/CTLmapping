0c755af18431cfbb01482a848c146561cc158c6f
DannyArends <Danny.Arends@gmail.com>
2012-09-13 18:58:04 +0200

    Updates to the statistics code whicg is too complex

--- Rctl/R/ctl.statistics.R
+++ Rctl/R/ctl.statistics.R
@@ -12,22 +12,21 @@
 CTLtoP <- function(CTLscan, onlySignificant = TRUE, verbose = TRUE){
   if(missing(CTLscan)) stop("argument 'CTLscan' is missing, with no default")
   permvalues <- sort(unlist(CTLscan$p))
-  l <- length(permvalues)
+  significant <- c()
   if(onlySignificant){
-    mysignificant <- as.numeric(which(apply(abs(CTLscan$ctl),1,max) > getPermuteThresholds(CTLscan$p)[1]))
-    if(length(mysignificant) > 1){
-      scaled <- abs(CTLscan$ctl[mysignificant, ])
-      rnames <- rownames(CTLscan$ctl)[mysignificant]
-    }else{
-      scaled <- abs(CTLscan$ctl)
-      rnames <- rownames(CTLscan$ctl)
-    }
+    maximums <- apply(abs(CTLscan$ctl),1,max)
+    significant <- as.numeric(which(maximums > getPermuteThresholds(CTLscan$p)[1]))
+  }  
+  if(length(significant) > 1){
+    scaled <- abs(CTLscan$ctl[significant, ])
+    rnames <- rownames(CTLscan$ctl)[significant]
   }else{
+    if(onlySignificant) warn("No significant found, converting all")
     scaled <- abs(CTLscan$ctl)
     rnames <- rownames(CTLscan$ctl)
   }
   pvalues <- unlist(lapply(1:length(permvalues),function(x){1-(x-1)/length(permvalues)}))
-  result <- apply(scaled, 2, function(x){CTLtoPvalue.internal(x, permvalues, pvalues, l)})
+  result <- apply(scaled, 2, function(x){CTLtoPvalue.internal(x, permvalues, pvalues)})
   rownames(result) <- rnames
   result
 }
@@ -37,54 +36,38 @@ CTLscoretoPvalue <- function(CTLscore, CTLpermute){
   if(missing(CTLpermute)) stop("argument 'CTLpermute' is missing, with no default")
   permvalues <- sort(unlist(CTLpermute))
   pvalues <- unlist(lapply(1:length(permvalues),function(x){1-(x-1)/length(permvalues)}))
-  l <- length(permvalues)
-  CTLtoPvalue.internal(CTLscore, permvalues, pvalues, l)
+  CTLtoPvalue.internal(CTLscore, permvalues, pvalues)
 }
 
 #Determine a P-value based on the relative position of the score within the permutations
 #Out of range values are tested using a GPD to estimate a P-value
-CTLtoPvalue.internal <- function(CTLscore, permvalues, pvalues, l = length(permvalues)){
+CTLtoPvalue.internal <- function(CTLscore, permvalues, pvalues){
   cv <- as.numeric(permvalues[1])
   permvalues <- as.numeric(permvalues)
   res <- unlist(lapply(CTLscore, function(y){
-    #cat("Critical value:", cv,"\n")
-    #cat("Request for:",y,"???",as.numeric(y) < as.numeric(cv),"\n")
     y <- as.numeric(y)
-    if(is.na(y)){
-      cat("NA\n")
-      return(1)
-    }
-   # cat("y:",y,"cv:", cv,"\n")
-    if(y < cv){
-      #cat("<cv = 0\n")
-      return(pvalues[1])
+    if(is.na(y)) return(1)
+    if(y < cv) return(pvalues[1])
+    myrange <- which(permvalues > y)
+    icx <- Inf
+    if(length(myrange) > 0) icx <- min(myrange)
+    if(is.finite(icx)){
+      if(pvalues[min(icx)]==0) stop("P value of 0.0 at: pvalues[min(icx)]")
+      return(pvalues[min(icx)])
     }else{
-      myrange <- which(permvalues > y)
-      icx <- Inf
-      if(length(myrange) > 0) icx <- min(myrange)
-      if(is.finite(icx)){
-        if(pvalues[min(icx)]==0) stop("P value of 0.0 at: pvalues[min(icx)]")
-#        cat("inrage: ",icx," ",y, "==>",pvalues[min(icx)],"\n")
-        return(pvalues[min(icx)])
-      }else{
-        tryCatch(estimate <- extrapolateBeyondRange(permvalues, y),  error = function(e) {estimate <<- 1})
-        cat("estimate: ",estimate,"\n")
-        best_p <- pvalues[length(pvalues)]
-        if(estimate > best_p){
-          if(best_p == 0) stop("P value of 0.0 at: 1-((l-1)/l)")
-          return(best_p)
-        }
-        if(estimate==0) stop("P value of 0.0 at: GPD estimation")
-        return(estimate)
-      }
+      tryCatch(estimate <- extrapolateBeyondRange(permvalues, y),  error = function(e) {estimate <<- 1})
+      cat("Y is out of Range estimate: ",estimate,"\n")
+      best_p <- pvalues[length(pvalues)]
+      if(estimate > best_p) return(best_p)
+      return(estimate)
     }
-    }))
+  }))
   res
 }
 
 #Use the top 10% of permutation scores and fit a GPD uppertail distribution, then use the 
-#GPD to obtain P-values for outliers, If the GPD is 0 we reduce our value till we get the 
-#minimum P-value
+#GPD to obtain P-values for outliers, If the GPD estimates a 0 p-value we reduce our value 
+#to get the minimum P-value
 extrapolateBeyondRange <- function(permvalues, value = 0.6, top = 20){
   require(POT)
   gpd.threshold <- permvalues[(.80*length(permvalues))]
@@ -100,21 +83,20 @@ extrapolateBeyondRange <- function(permvalues, value = 0.6, top = 20){
     warn <- TRUE
     value <- value - 0.0001
   }
-  #if(warn){
-  ##  cat("Warning: scores out of permutation range, unable to estimate correctly",value,"/",prev.value,dens(value),"\n")
-  #}
   as.numeric(dens(value))
 }
 
 toLod <- function(CTLscan, onlySignificant = TRUE, verbose = TRUE){
+  if(missing(CTLscan)) stop("argument 'CTLscan' is missing, with no default")
   ss <- proc.time()
-  pmatrix <- CTLtoP(CTLscan, onlySignificant, verbose)
+  pm <- CTLtoP(CTLscan, onlySignificant, verbose)
   ee <- proc.time()
   if(verbose) cat("  - toLOD took",as.numeric(ee[3]-ss[3]),"seconds\n")
-  -log10(pmatrix)
+  -log10(pm)
 }
 
 CTLtoLODvector <- function(CTLscan, against = c("markers","phenotypes")){
+  if(missing(CTLscan)) stop("argument 'CTLscan' is missing, with no default")
   if(!is.null(CTLscan$l)){
     if(against[1]=="markers")return(apply(CTLscan$l,2,sum))
     if(against[1]=="phenotypes")return(apply(CTLscan$l,1,max))
