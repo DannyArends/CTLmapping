0e582a75af99c59f996059c2a27112d1e5e345b6
DannyArends <Danny.Arends@gmail.com>
2012-10-13 22:07:55 +0200

    Minor updates fixed the tests

--- Rctl/tests/GeneExpression_Yeast_RBrem.R
+++ Rctl/tests/GeneExpression_Yeast_RBrem.R
@@ -13,15 +13,11 @@ if(has_rqtl()){
   cross <- read.cross("csvr",file="GeneExpression_Yeast_RBrem.csvr",geno=c("AA","AB"))
   cross <- convert2riself(cross)
 
-  CTL <- CTLscan.cross(cross,1, verbose=T)
+  CTL <- CTLscan.cross(cross, 1:5, verbose=T)
   image(CTL)
   plot(CTL)
 
-  nodes <- CTLnetwork(CTL,0.55)
-
-  write.nodeAttributeFile(nodes,probeannot)
-  genes <- nodesToGenes(nodes,probeannot)
-  pathwaydata <- download.KEGG.annotation(genes,verb=T)
+  nodes <- CTLnetwork(CTL, significance = 0.1)
 }else{
   cat("Test skipped because of missing R/qtl package")
 }
