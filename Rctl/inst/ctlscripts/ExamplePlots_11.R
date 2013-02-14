69950b2a9e24ecdd1094e22710714d85715ad9f9
DannyArends <Danny.Arends@gmail.com>
2013-02-14 14:49:14 +0100

    Minor updates to the last row of emaple plots, no more variation

--- Rctl/inst/ctlscripts/ExamplePlots_11.R
+++ Rctl/inst/ctlscripts/ExamplePlots_11.R
@@ -24,7 +24,7 @@ mb <- (runif(1000)-0.5)/2
   #A) QTL QTL, CTL
   plot(c(-3.5,3.5),c(-3.5,3.5),t='n', xlab="", ylab="", main="",xaxt='n',yaxt='n')
   points(sort(aa)+0.5, bb-1, pch=pchaa,col='black')
-  points(sort(aa)+ma-1,sort(bb)+mb+.5,pch=pchbb,col="gray")
+  points(sort(aa)+ma-1,sort(bb)+mb+.5,pch=pchbb,col="orange")
   legend("topleft", c("A)"),bty='n')
 
 aa <- rnorm(1000)
@@ -35,7 +35,7 @@ mb <- (runif(1000)-0.5)/2
   #A) QTL, CTL
   plot(c(-3.5,3.5),c(-3.5,3.5),t='n', xlab="", ylab="", main="",xaxt='n',yaxt='n')
   points(sort(aa), bb-1, pch=pchaa,col='black')
-  points(sort(aa)+ma,sort(bb)+mb+.5,pch=pchbb,col="gray")
+  points(sort(aa)+ma,sort(bb)+mb+.5,pch=pchbb,col="orange")
   legend("topleft", c("B)"),bty='n')
 
 aa <- rnorm(1000)
@@ -46,7 +46,7 @@ mb <- (runif(1000)-0.5)/2
   #A) QTL, CTL
   plot(c(-3.5,3.5),c(-3.5,3.5),t='n', xlab="", ylab="", main="",xaxt='n',yaxt='n')
   points(sort(aa)-1, bb, pch=pchaa,col='black')
-  points(sort(aa)+ma+0.5,sort(bb)+mb,pch=pchbb,col="gray")
+  points(sort(aa)+ma+0.5,sort(bb)+mb,pch=pchbb,col="orange")
   legend("topleft", c("B)"),bty='n')
 
 aa <- rnorm(1000)
@@ -57,7 +57,7 @@ mb <- (runif(1000)-0.5)/2
   #B) QTL, CTL - Genotype AA ON
   plot(c(-3.5,3.5),c(-3.5,3.5),t='n', xlab="", ylab="", main="",xaxt='n',yaxt='n')
   points(aa, bb, pch=pchaa,col='black')
-  points(sort(aa)+ma,sort(bb)+mb,pch=pchbb,col="gray")
+  points(sort(aa)+ma,sort(bb)+mb,pch=pchbb,col="orange")
   legend("topleft", c("C)"),bty='n')
 
 aa <- rnorm(1000)
@@ -71,7 +71,7 @@ aa <- rnorm(1000)
 bb <- rnorm(1000)
 ma <- (runif(1000)-0.5)/2
 mb <- (runif(1000)-0.5)/2
-  points(bb+ma-0.7, (0.2*aa)+(aa/2)+0.7, pch=pchbb,col='gray')
+  points(bb+ma-0.7, (0.2*aa)+(aa/2)+0.7, pch=pchbb,col='orange')
   legend("topleft", c("D)"),bty='n')
 
 
@@ -86,7 +86,7 @@ aa <- rnorm(1000)
 bb <- rnorm(1000)
 ma <- (runif(1000)-0.5)/2
 mb <- (runif(1000)-0.5)/2
-  points(bb+ma, (0.2*aa)+(aa/2)+0.7, pch=pchbb,col='gray')
+  points(bb+ma, (0.2*aa)+(aa/2)+0.7, pch=pchbb,col='orange')
   legend("topleft", c("D)"),bty='n')
   #legend("topright", c("No QTL T1","QTL T2","No CTL T1:T2"),bty='n')
 
@@ -96,7 +96,7 @@ aa <- rnorm(1000)
 bb <- rnorm(1000)
 ma <- (runif(1000)-0.5)/2
 mb <- (runif(1000)-0.5)/2
-  points(bb+ma-0.7, (0.2*aa)+(aa/2), pch=pchbb,col='gray')
+  points(bb+ma-0.7, (0.2*aa)+(aa/2), pch=pchbb,col='orange')
   legend("topleft", c("D)"),bty='n')
   #legend("topright", c("No QTL T1","QTL T2","No CTL T1:T2"),bty='n')
 
@@ -108,7 +108,7 @@ bb <- rnorm(1000)
 ma <- (runif(1000)-0.5)/2
 mb <- (runif(1000)-0.5)/2
 
-  points(bb+ma, aa, pch=pchbb,col='gray')
+  points(bb+ma, aa, pch=pchbb,col='orange')
   points(bb[1:50], aa[1:50], pch=pchaa,col='black')
   legend("topleft", c("D)"),bty='n')
 
@@ -119,27 +119,28 @@ mb <- (runif(1000)-0.5)/2
 
   #A) QTL, CTL - Genotype AA OFF
   plot(c(-3.5,3.5),c(-3.5,4.5),t='n', xlab="", ylab="", main="",xaxt='n',yaxt='n')
-  points(sort(aa)+ma,sort(bb)+mb+1.7, pch=pchaa,col='gray')
-  points(bb+ma, (0.1*aa)-2.5, pch=pchbb,col='black')
+  points(sort(aa)+ma,sort(bb)+mb+1.3, pch=pchaa,col='orange')
+  points(bb+ma, rep(-2, 1000), pch=pchbb,col='black')
   legend("topleft", c("E)"),bty='n')
 
   #A) QTL, CTL - Genotype BB OFF
   plot(c(-3.5, 4.5),c(-3.5,3.5),t='n', xlab="", ylab="", main="",xaxt='n',yaxt='n')
-  points(sort(aa)+ma+1.7,sort(bb)+mb+.3, pch=pchaa,col='black')
-  points((0.1*aa)-2.5,bb+ma, pch=pchbb,col='gray')
+  points(sort(aa)+ma+1.3,sort(bb)+mb+.3, pch=pchaa,col='black')
+  points(rep(-2, 1000),bb+ma, pch=pchbb,col='orange')
   legend("topleft", c("E)"),bty='n')
 
   #A) QTL, CTL - Genotype AA an BB OFF
   plot(c(-3.5,4.5),c(-3.5,4.5),t='n', xlab="", ylab="", main="",xaxt='n',yaxt='n')
-  points((0.1*aa)-2.5,bb+ma+1, pch=pchaa,col='gray')
-  points(bb+ma+1, (0.1*aa)-2.5, pch=pchbb,col='black')
-  legend("topleft", c("E)"),bty='n')
+  #points((0.1*aa)-2.5,bb+ma+1, pch=pchaa,col='orange')
+  #points(bb+ma+1, rep(-2, 1000), pch=pchbb,col='black')
+  #legend("topleft", c("E)"),bty='n')
 
   #A) QTL, CTL - Genotype BB OFF
   plot(c(-3.5,4.5),c(-3.5,4.5),t='n', xlab="", ylab="", main="",xaxt='n',yaxt='n')
-  points(sort(aa)+ma+1.7,sort(bb)+mb+1.7, pch=pchaa,col='gray')
+  points(sort(aa)+ma+1.7,sort(bb)+mb+1.7, pch=pchaa,col='orange')
   points(0.2*aa-2.3,0.2*bb-2.3, pch=pchbb,col='black')
   legend("topleft", c("E)"),bty='n')
+
 
 
   #legend("topleft", c("No QTL T1","QTL T2","CTL T1:T2"),bty='n')
