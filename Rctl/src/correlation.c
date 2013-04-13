00246e8f6748f38e34ef8a00cc594956f9ae8c05
DannyArends <Danny.Arends@gmail.com>
2013-04-13 16:22:59 +0200

    Minor updates, removing some old code (ChiSQ and ctleffectsOLD)

--- Rctl/src/correlation.c
+++ Rctl/src/correlation.c
@@ -10,26 +10,22 @@
 
 double correlation(const double* x, const double* y, size_t dim, bool verbose){
   size_t i;
+  double XiYi = 0.0, Xi = 0.0, Yi = 0.0, XiP2 = 0.0, YiP2 = 0.0;
   double onedivn = 1.0 / dim;
-  KahanAccumulator XiYi = createAccumulator();
-  KahanAccumulator Xi   = createAccumulator();
-  KahanAccumulator Yi   = createAccumulator();
-  KahanAccumulator XiP2 = createAccumulator();
-  KahanAccumulator YiP2 = createAccumulator();
 
   for(i = 0; i < dim; i++){
     if(x[i] != MISSING && y[i] != MISSING){
-      XiYi = KahanSum(XiYi, x[i] * y[i]);
-      Xi   = KahanSum(Xi, x[i]);
-      Yi   = KahanSum(Yi, y[i]);
-      XiP2 = KahanSum(XiP2, x[i] * x[i]);
-      YiP2 = KahanSum(YiP2, y[i] * y[i]);
+      XiYi += x[i] * y[i];
+      Xi   += x[i];
+      Yi   += y[i];
+      XiP2 += x[i] * x[i];
+      YiP2 += y[i] * y[i];
     }
   }
-  double nom = (XiYi.sum - (onedivn*Xi.sum*Yi.sum));
-  double denom = sqrt(XiP2.sum - onedivn * pow(Xi.sum, 2.0)) * sqrt(YiP2.sum - onedivn * pow(Yi.sum, 2.0));
-  double cor = nom/denom;
-  if(isNaN(cor) || isinf(cor) || cor < -1 || cor > 1){ 
+  double nom = (XiYi - (onedivn *Xi * Yi));
+  double denom = sqrt(XiP2 - onedivn * pow(Xi, 2.0)) * sqrt(YiP2 - onedivn * pow(Yi, 2.0));
+  double cor = nom / denom;
+  if(isNaN(cor) || isinf(cor) || cor < (-1.0 + EPSILON) || cor > (1.0 + EPSILON)){ 
     err("Correlation '%.4f' not in range [-1, 1]\n", cor); 
   }
   return(cor);
@@ -37,10 +33,10 @@ double correlation(const double* x, const double* y, size_t dim, bool verbose){
 
 double* cor1toN(double* x, double** y, size_t dim, size_t ny, bool verbose){
   size_t i, j;
-  double onedivn = 1.0 / dim;
-  double Xi      = 0.0;
-  double XiP2    = 0.0;
+  double nom, denom;
+  double onedivn = (1.0 / dim), Xi = 0.0, XiP2 = 0.0;
 
+  double* cors   = newdvector(ny);
   double* Yi     = newdvector(ny);
   double* YiP2   = newdvector(ny);
   double* XiYi   = newdvector(ny);
@@ -57,15 +53,13 @@ double* cor1toN(double* x, double** y, size_t dim, size_t ny, bool verbose){
       YiP2[j] += y[j][i] * y[j][i];
     }}
   }
-  double nom, denom;
-  double* cors = newdvector(ny);
 
   for(j = 0; j < ny; j++){
     nom   = (XiYi[j] - (onedivn*Xi*Yi[j]));
     denom = sqrt(XiP2 - (onedivn * Xi * Xi)) * sqrt(YiP2[j] - (onedivn * Yi[j] * Yi[j]));
-    if(denom == 0) err("Empty denominator in correlation (Too few samples in a genotype)\n");
+    if(denom == 0) err("Denominator = 0 in correlation (Too few samples in a genotype)\n");
     cors[j] = nom / denom;
-    if(isNaN(cors[j]) || isinf(cors[j]) || cors[j] < -1.0 + EPSILON || cors[j] > 1.0 + EPSILON){ 
+    if(isNaN(cors[j]) || isinf(cors[j]) || cors[j] < (-1.0 + EPSILON) || cors[j] > (1.0 + EPSILON)){ 
       err("Correlation '%.8f' not in range [-1, 1]\n", cors[j]);
     }
   }
@@ -77,7 +71,7 @@ double* getCorrelations(const Phenotypes phenotypes, const Genotypes genotypes, 
                         clvector genoenc, size_t mar, size_t phe2, bool verbose){
 
   size_t  i;
-  double* cors  = newdvector(genoenc.nelements);
+  double* cors = newdvector(genoenc.nelements);
   if(phe1 != phe2){
     for(i = 0; i < genoenc.nelements; i++){
       clvector inds = which(genotypes.data[mar], phenotypes.nindividuals, genoenc.data[i]);
@@ -97,7 +91,7 @@ double* getCorrelations(const Phenotypes phenotypes, const Genotypes genotypes, 
 
 double* chiSQN(size_t nr, double** r, size_t phe, int* nsamples, size_t nphe){
   size_t p, i, denom;
-  double sumOfSquares, squaresOfSum, df;
+  double sumOfSquares = 0.0, squaresOfSum = 0.0, df;
   double* ret = newdvector(nphe);  /*!< Returned Chi^2 values for phenotype phe against the other phenotypes */
   for(p = 0; p < nphe; p++){
     if(phe != p){
@@ -108,30 +102,13 @@ double* chiSQN(size_t nr, double** r, size_t phe, int* nsamples, size_t nphe){
           sumOfSquares += df * pow(zscore(r[i][p]), 2.0);
           squaresOfSum += df * zscore(r[i][p]);
           denom += df;
-        }else{ err("Correlation '%d' between %d and %d has too few observations", i, p, phe); }
+        }
       }
       if(denom == 0) err("Divide by 0 groups too small");
       ret[p] = sumOfSquares - (pow(squaresOfSum, 2.0) / denom);
     }
   }
   return ret;
-}
-
-double chiSQ(size_t nr, double* r, int* nsamples){
-  size_t i;
-  size_t denom = 0;
-  KahanAccumulator sumOfSquares = createAccumulator();
-  KahanAccumulator squaresOfSum = createAccumulator();
-
-  for(i = 0; i < nr; i++){
-    if(nsamples[i] > 3){
-      sumOfSquares = KahanSum(sumOfSquares, (nsamples[i]-3) * pow(zscore(r[i]), 2.0));
-      squaresOfSum = KahanSum(squaresOfSum, (nsamples[i]-3) * zscore(r[i]));
-      denom  += (nsamples[i]-3);
-    }
-  }
-  if(denom == 0) err("Divide by 0 groups too small");
-  return(sumOfSquares.sum - (pow(squaresOfSum.sum, 2.0) / denom));
 }
 
 double chiSQtoP(double Cv, int Dof){
