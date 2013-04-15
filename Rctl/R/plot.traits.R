4675843e9f217a9efbbf6d131f7aeeb25fa32341
DannyArends <Danny.Arends@gmail.com>
2013-04-15 10:31:19 +0200

    Removing some unreliable code, better name

---
+++ Rctl/R/plot.traits.R
@@ -0,0 +1,41 @@
+#
+# plot.traits.R
+#
+# copyright (c) 2010-2012 - GBIC, Danny Arends, Bruno Tesson and Ritsert C. Jansen
+# last modified Apr, 2013
+# first written Apr, 2013
+# 
+# Trait vs Trait scatter plot routine for CTL analysis
+#
+
+plot.traits <- function(genotypes, phenotypes, pheno.col = c(1, 2), marker = 1, doLog = FALSE){
+  t1 <- phenotypes[, pheno.col[1]]
+  t2 <- phenotypes[, pheno.col[2]]
+  if(doLog){ t1 <- log10(t1); t2 <- log10(t2) }
+
+  geno  <- genotypes[,marker]
+
+  betas <- stdSlopeEstimate(t1,t2, geno)
+  inter <- stdSlopeIntercept(t1,t2, geno, betas)
+
+  t1name <- colnames(phenotypes)[pheno.col[1]]
+  t2name <- colnames(phenotypes)[pheno.col[2]]
+
+  plot(t1, t2, type = "n", main = "Trait-Trait scatterplot", xlab=t1name, ylab=t2name)
+  max1 <- max(t1*1.25, na.rm=TRUE);  max2 <- max(t2*1.25, na.rm=TRUE)
+  min1 <- min(t1, na.rm=TRUE);       min2 <- min(t2, na.rm=TRUE)
+  abline( v = seq(min1, max1, (max1 - min1) / 40), lty = 3, col = colors()[ 440 ] )
+  abline( h = seq(min2, max2, (max2 - min2) / 40), lty = 3, col = colors()[ 440 ] )
+
+  points(t1, t2, pch=19, cex=0.5, col = geno)
+  cors <- NULL
+  for(x in 1:length(unique(geno))){
+    idx <- which(geno == unique(geno)[x])
+    cors <- c(cors, cor(t1[idx], t2[idx],use="pair"))
+    abline(inter[x], betas[x], col = x, lwd=1.5)
+  }
+  legend("topright", title="Correlation", col=unique(geno), legend = round(cors, d=2),lwd=1, cex=0.7)
+  legend("topleft",  title="Slope", col=unique(geno), legend = round(cors, d=2),lwd=1, cex=0.7)
+}
+
+# end of plot.traits.R
