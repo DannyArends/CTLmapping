db5c4466d7d451395e4ff2fa68d7fd90e883302b
DannyArends <danny.arends@gmail.com>
2016-04-04 09:35:21 +0200

    Updated the pheno.col to phenocol as to not have a dot in the parameter

--- Rctl/R/ctl.scan.R
+++ Rctl/R/ctl.scan.R
@@ -9,18 +9,18 @@
 #
 
 #-- CTLscan main function --#
-CTLscan <- function(genotypes, phenotypes, pheno.col, nperm = 100, strategy = c("Exact", "Full", "Pairwise"), conditions = NULL, qtls = NULL, ncores = 1, parametric = FALSE, verbose = FALSE){
+CTLscan <- function(genotypes, phenotypes, phenocol, nperm = 100, strategy = c("Exact", "Full", "Pairwise"), conditions = NULL, qtls = NULL, ncores = 1, parametric = FALSE, verbose = FALSE){
   st <- proc.time()
   if(missing(genotypes) || is.null(genotypes))  stop("argument 'genotypes' is missing, with no default")
   if(any(class(genotypes)=="cross")){
-    CTLscan.cross(genotypes, pheno.col=pheno.col, nperm=nperm, strategy=strategy, conditions=conditions,
+    CTLscan.cross(genotypes, phenocol=phenocol, nperm=nperm, strategy=strategy, conditions=conditions,
                   qtls=qtls, ncores=ncores, parametric = parametric, verbose=verbose)
   }
   if(missing(phenotypes)|| is.null(phenotypes)) stop("argument 'phenotypes' is missing, with no default")
-  if(missing(pheno.col)) pheno.col = 1:ncol(phenotypes)
+  if(missing(phenocol)) phenocol = 1:ncol(phenotypes)
   n.phe      <- ncol(phenotypes); n.ind1 <- nrow(phenotypes)
   n.mar      <- ncol(genotypes);  n.ind2 <- nrow(genotypes)
-  if(!is.null(qtls) && ncol(qtls) != length(pheno.col)) stop("Number of QTLs doesn't match")
+  if(!is.null(qtls) && ncol(qtls) != length(phenocol)) stop("Number of QTLs doesn't match")
   if(n.ind1 != n.ind2) stop("Number of individuals doesn't match between genotypes & phenotypes")
   if(n.phe < 2) stop("argument 'phenotypes' needs at least 2 columns")
   if(verbose){
@@ -31,22 +31,22 @@ CTLscan <- function(genotypes, phenotypes, pheno.col, nperm = 100, strategy = c(
     phenotypes <- apply(phenotypes, 2, rank) # Parametric vs Non-Parametric testing
     if(verbose) cat("Data ranking finished after:",(proc.time()-st)[3],"seconds\n")
   }
-  results    <- vector("list",length(pheno.col))
+  results    <- vector("list",length(phenocol))
 
   if(ncores == 1){ # Single core debug call
     idx <- 1
-    for(phe in pheno.col){
-      results[[idx]] <- CTLmapping(genotypes, phenotypes, pheno.col=phe, nperm=nperm,
+    for(phe in phenocol){
+      results[[idx]] <- CTLmapping(genotypes, phenotypes, phenocol=phe, nperm=nperm,
                              strategy=strategy, qtls=qtls, verbose=verbose)
       idx <- idx + 1
     }
   }else{ # Normally we would want to use multiple cores
-    min.cores <- min(ncores,length(pheno.col))
-    if(min.cores != ncores) warning("Reduced n.cores (",ncores,"to",min.cores,")")
+    min.cores <- min(ncores, length(phenocol))
+    if(min.cores != ncores) warning("Reduced n.cores (", ncores, "to", min.cores, ")")
     cl <- parallel::makeCluster(rep("localhost", min.cores))
     parallel::clusterEvalQ(cl, library(ctl))
-    results <- parallel::parLapply(cl, pheno.col, function(x, qtls){
-      CTLmapping(genotypes, phenotypes, pheno.col=x, nperm=nperm, strategy=strategy,
+    results <- parallel::parLapply(cl, phenocol, function(x, qtls){
+      CTLmapping(genotypes, phenotypes, phenocol=x, nperm=nperm, strategy=strategy,
                  qtls = qtls, verbose=verbose)
     }, qtls)
     parallel::stopCluster(cl)
@@ -56,7 +56,7 @@ CTLscan <- function(genotypes, phenotypes, pheno.col, nperm = 100, strategy = c(
   invisible(results)
 }
 
-CTLmapping <- function(genotypes, phenotypes, pheno.col = 1, nperm = 100, strategy = c("Exact", "Full", "Pairwise"), qtls = NULL, verbose = FALSE){
+CTLmapping <- function(genotypes, phenotypes, phenocol = 1, nperm = 100, strategy = c("Exact", "Full", "Pairwise"), qtls = NULL, verbose = FALSE){
   if(missing(genotypes) || is.null(genotypes)) stop("argument 'genotypes' is missing, with no default")
   if(missing(phenotypes)|| is.null(phenotypes)) stop("argument 'phenotypes' is missing, with no default")
 
@@ -64,12 +64,12 @@ CTLmapping <- function(genotypes, phenotypes, pheno.col = 1, nperm = 100, strate
   res <- list()
   ss  <- proc.time()
   if(!is.null(qtls)){
-    res$qtl <- qtls[,pheno.col]
+    res$qtl <- qtls[,phenocol]
   }else{
     res$qtl <- rep(0,ncol(genotypes))
     tryCatch( #Maps QTL profile using a relatively 'slow' approach
       res$qtl <- apply(genotypes, 2, function(x){
-                   -log10(anova(lm(phenotypes[,pheno.col] ~ x))[[5]][1])
+                   -log10(anova(lm(phenotypes[,phenocol] ~ x))[[5]][1])
                  }), error = function(e) e)
   }
 
@@ -88,7 +88,7 @@ CTLmapping <- function(genotypes, phenotypes, pheno.col = 1, nperm = 100, strate
 
   result <- .C("R_mapctl",as.integer(n.ind), as.integer(n.mar), as.integer(n.phe),
                           as.integer(unlist(genotypes)), as.double(unlist(phenotypes)),
-                          as.integer((pheno.col-1)), as.integer(nperm),
+                          as.integer((phenocol-1)), as.integer(nperm),
                           as.integer(perm.type),
                           dcor =as.double(rep(0,n.mar*n.phe)),
                           perms=as.double(perms),
@@ -104,11 +104,11 @@ CTLmapping <- function(genotypes, phenotypes, pheno.col = 1, nperm = 100, strate
   if(any(is.na(res$dcor))) warning("Differential correlation: NaN scores, no variance ?")
   rownames(res$dcor) <- colnames(genotypes); colnames(res$dcor) <- colnames(phenotypes)
   rownames(res$ctl)  <- colnames(genotypes); colnames(res$ctl)  <- colnames(phenotypes)
-  attr(res,"name") <- colnames(phenotypes)[pheno.col]
+  attr(res,"name") <- colnames(phenotypes)[phenocol]
   class(res) <- c(class(res),"CTLscan")
   if(verbose){
     t1 <- as.numeric(e2[3]-e1[3]) ; t2 <- as.numeric(e1[3]-ss[3])
-    cat("Phenotype ",pheno.col,": Done after ", t1," ", t2," seconds\n", sep="")
+    cat("Phenotype ",phenocol,": Done after ", t1," ", t2," seconds\n", sep="")
   }
   invisible(res)
 }
