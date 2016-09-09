6dac4292be831b634baabeca850c1cc6c07bf5d1
DannyArends <danny.arends@gmail.com>
2016-09-09 10:50:02 +0200

    Adding the getCorrelations function to memleak, so valgrind can test it

--- examples/memleak.R
+++ examples/memleak.R
@@ -1,4 +1,7 @@
 library(ctl)
 data(ath.metabolites)                 # Arabidopsis Thaliana data set
 ctlscan <- CTLscan(ath.metab$genotypes, ath.metab$phenotypes, phenocol=1:4)
+crs <- getCorrelations(ath.metab$genotypes, ath.metab$phenotypes)
+cvs <- chiSQN(crs$correlations, crs$samplesize)
+pvs <- chiSQtoP(cvs, (length(crs$samplesize)-1))
 q("no")
